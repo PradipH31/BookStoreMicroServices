953cfac58ae188eb65dd7f5b7db3ab4a2c7a5ae1
PradipH31 <pradip.hamal31@gmail.com>
2023-10-02 09:19:02 -0500

    Small modifications for fixing services

--- review-service/src/main/java/com/pradiph31/reviewservice/entities/Review.java
+++ review-service/src/main/java/com/pradiph31/reviewservice/entities/Review.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "books")
+@Document(collection = "reviews")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- review-service/src/main/java/com/pradiph31/reviewservice/services/ReviewServiceImpl.java
+++ review-service/src/main/java/com/pradiph31/reviewservice/services/ReviewServiceImpl.java
@@ -3,9 +3,11 @@ package com.pradiph31.reviewservice.services;
 import com.pradiph31.reviewservice.entities.Review;
 import com.pradiph31.reviewservice.repositories.ReviewRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @AllArgsConstructor
+@Service
 public class ReviewServiceImpl implements ReviewService{
 
     private ReviewRepository reviewRepository;
